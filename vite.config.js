9e2e2e84863e23daad19b17f8d8e1f1a121f0ceb
Nicholas Wilkins <150397998+Darth-Volix@users.noreply.github.com>
2025-03-31 23:56:27 -0600

    configure Vite build settings with output directory and entry point

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,15 @@
-import { defineConfig } from 'vite';
+import { resolve } from "path";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  root: 'src',
+  root: "src/",
+
+  build: {
+    outDir: "../dist",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "src/index.html")
+      }
+    }
+  }
 });
