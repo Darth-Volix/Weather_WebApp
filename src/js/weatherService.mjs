68194f69fb5426c320e26588743d68ab8cade05d
Nicholas Wilkins <150397998+Darth-Volix@users.noreply.github.com>
2025-03-31 20:02:57 -0600

    refactor weatherService.mjs to correct API URL usage and add current weather data retrieval function

--- src/js/weatherService.mjs
+++ src/js/weatherService.mjs
@@ -5,6 +5,7 @@ const apiKey = import.meta.env.VITE_ACCUWEATHER_API_KEY;
 const baseURLPostalCode = "http://dataservice.accuweather.com/locations/v1/postalcodes/search?";
 const baseURLCurrentConditions = "http://dataservice.accuweather.com/currentconditions/v1/";
 
+
 // Function to fetch data.
 async function getData(url) {
     const options = {
@@ -16,7 +17,7 @@ async function getData(url) {
   
     let data = null; 
   
-    const response = await fetch(baseURL + url, options);
+    const response = await fetch(url, options);
   
     if (response.ok) {
       data = await response.json();
@@ -37,10 +38,18 @@ function findLocationKey(locationData, cityName, cityState) {
 
 // Function to get location key based on postal code
 async function getLocationKey(cityName, cityState, postalCode) {
-    const url = "&q=" + postalCode;
+    const url = baseURLPostalCode + "&q=" + postalCode;
     const locationData = await getData(url);
 
     const result = findLocationKey(locationData, cityName, cityState);
 
     return result;
+}
+
+// Function to get the current weather condition data
+async function getCurrentWeather(locationKey) {
+    const url = baseURLCurrentConditions + locationKey + "?&details=true";
+    const currentWeatherData = await getData(url);
+
+    return currentWeatherData;
 }
